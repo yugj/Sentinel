623cfa3e97ce4870ac1bda70ba7c1514e224871a
yugj <yugj@hotmail.com>
2019-09-10 16:50:12 +0800

    config file

--- sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/zookeeper/ZookeeperConfig.java
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/zookeeper/ZookeeperConfig.java
@@ -21,6 +21,7 @@ import com.alibaba.fastjson.JSON;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -28,6 +29,9 @@ import java.util.List;
 
 @Configuration
 public class ZookeeperConfig {
+
+    @Value("${rule.repository.zookeeper.nodes}")
+    private String zkNodes;
 
     @Bean
     public Converter<List<FlowRuleEntity>, String> flowRuleEntityEncoder() {
@@ -42,7 +46,7 @@ public class ZookeeperConfig {
     @Bean
     public CuratorFramework zkClient() {
         CuratorFramework zkClient =
-                CuratorFrameworkFactory.newClient("127.0.0.1:2181",
+                CuratorFrameworkFactory.newClient(zkNodes,
                         new ExponentialBackoffRetry(ZookeeperConfigUtil.SLEEP_TIME, ZookeeperConfigUtil.RETRY_TIMES));
         zkClient.start();
 
